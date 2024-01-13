784c962ecf74f7b91c1d9e36c2efd6d9c21b20a3
Goran <goran.gratte@sortapp.co>
2024-01-13 15:40:09 +0100

    Initial Commit

---
+++ GHFollowers/SecondScreen.swift
@@ -0,0 +1,29 @@
+//
+//  SecondScreen.swift
+//  GHFollowers
+//
+//  Created by Göran Gratte on 2024-01-13.
+//
+
+import UIKit
+
+class SecondScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemMint
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
