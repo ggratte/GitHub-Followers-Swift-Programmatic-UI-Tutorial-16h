860db4ffda3fad5f85b5697c55634ca63fa1bf81
Goran <goran.gratte@sortapp.co>
2024-01-14 08:10:17 +0100

    Add Title and Body Label

---
+++ GHFollowers/GFBodyLabel.swift
@@ -0,0 +1,38 @@
+//
+//  GCBodyLabel.swift
+//  GHFollowers
+//
+//  Created by Göran Gratte on 2024-01-14.
+//
+
+import UIKit
+
+class GFBodyLabel: UILabel {
+
+    override init(frame: CGRect){
+        
+        super.init(frame: frame)
+
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(code:) has not been implemented")
+    }
+    
+    init(textAlignment: NSTextAlignment){
+        super.init(frame: .zero)
+        self.textAlignment = textAlignment
+        configure()
+    }
+    
+    private func configure(){
+        textColor = .secondaryLabel
+        font = UIFont.preferredFont(forTextStyle: .body)
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.75
+        lineBreakMode = .byWordWrapping
+        translatesAutoresizingMaskIntoConstraints = false
+        
+    }
+
+}
