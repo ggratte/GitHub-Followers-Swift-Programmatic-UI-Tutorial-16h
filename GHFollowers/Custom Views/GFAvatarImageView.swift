83d42d463e8d9f492a4a392825c9a16508b85b91
Goran <goran.gratte@sortapp.co>
2024-01-15 11:14:38 +0100

    add FollowerCell
    progress 4:32 in tutorial

---
+++ GHFollowers/Custom Views/GFAvatarImageView.swift
@@ -0,0 +1,32 @@
+//
+//  GFAvatarImageView.swift
+//  GHFollowers
+//
+//  Created by Göran Gratte on 2024-01-15.
+//
+
+import UIKit
+
+class GFAvatarImageView: UIImageView {
+    
+    let placeholderImage = UIImage(named: "avatar-placeholder")
+
+    override init(frame: CGRect){
+        
+        super.init(frame: frame)
+        configure()
+
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(code:) has not been implemented")
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        image = placeholderImage
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+
+}
