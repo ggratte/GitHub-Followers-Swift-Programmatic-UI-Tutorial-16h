6ce2e54c7553fa59ea4a9270ee257ba8ace008d7
Goran <goran.gratte@sortapp.co>
2024-01-15 05:40:12 +0100

    Add user and follower model

---
+++ GHFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by Göran Gratte on 2024-01-15.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
