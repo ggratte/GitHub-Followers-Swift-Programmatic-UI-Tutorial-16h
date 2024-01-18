4d9f0f8bd3a3eea4892f02c2664f8594db50e904
Goran <goran.gratte@sortapp.co>
2024-01-18 13:43:22 +0100

    add header to UserInfoVC

---
+++ GHFollowers/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GHFollowers
+//
+//  Created by Göran Gratte on 2024-01-18.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
