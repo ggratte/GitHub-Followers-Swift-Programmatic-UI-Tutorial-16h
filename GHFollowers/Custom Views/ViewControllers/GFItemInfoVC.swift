1d98521512e013e3108f7e07492b8a7344a0d9c2
Goran <goran.gratte@sortapp.co>
2024-01-19 02:32:39 +0100

    Add GFItemInfoView
    Also started with GFItemInfoVC

---
+++ GHFollowers/Custom Views/ViewControllers/GFItemInfoVC.swift
@@ -0,0 +1,18 @@
+//
+//  GFItemInfoVC.swift
+//  GHFollowers
+//
+//  Created by Göran Gratte on 2024-01-19.
+//
+
+import UIKit
+
+class GFItemInfoVC: UIViewController {
+let stackView = UIStackView()
+    let itemInforViewOne = GFItemInfoView
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
