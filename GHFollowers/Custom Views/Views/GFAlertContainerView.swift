2aa2857e2510b36910bfd0f1f4f18484fe0cef1f
Goran <goran.gratte@sortapp.co>
2024-01-21 06:03:34 +0100

    extract containerView from GFAlertVC

---
+++ GHFollowers/Custom Views/Views/GFAlertContainerView.swift
@@ -0,0 +1,29 @@
+//
+//  GFAlertContainerView.swift
+//  GHFollowers
+//
+//  Created by Göran Gratte on 2024-01-21.
+//
+
+import UIKit
+
+class GFAlertContainerView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(code:) has not been implemented")
+    }
+    
+    private func configure() {
+        backgroundColor = .systemBackground
+        layer.cornerRadius = 16
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.white.cgColor
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+
+}
